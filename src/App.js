9777b434a4ae359b737e5c191d689c8120d5581e
Chodavarapu Phani Visweswara Sandeep <phanivsandeep@gmail.com>
2025-03-24 18:27:57 -0400

    Vercel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Clipboard from './components/Clipboard';
 import { ThemeProvider } from './context/ThemeContext';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <ThemeProvider>
       <div className="app dark:bg-gray-900 transition-colors duration-200">
         <Clipboard />
+        <Analytics/>
       </div>
     </ThemeProvider>
   );
